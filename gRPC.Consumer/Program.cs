07d8206c80f2a8ef586b0c8f92db8183d61bbfcb
Mohamed Orabi <moh.orabi97@gmail.com>
2021-10-04 17:17:38 +0200

    Add project files.

---
+++ gRPC.Consumer/Program.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+using GrpcService;
+using System;
+
+namespace gRPC.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            Greeter.GreeterClient client = new Greeter.GreeterClient(channel);
+
+            HelloRequest request = new HelloRequest { Name = "Hello My name is Mohamed Orabi" };
+            var response =  client.SayHello(request);
+            Console.WriteLine("From Grpc Service " + response);
+            Console.ReadLine();
+        }
+    }
+}
